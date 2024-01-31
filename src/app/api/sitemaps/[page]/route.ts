bdce2b7de334dc72d9ffc05cfd10d5858cfd9d01
Jordan Plows <jordan@lightapi.com>
2024-01-30 23:24:25 -0800

    dev: updating the sitemap

--- src/app/api/sitemaps/[page]/route.ts
+++ src/app/api/sitemaps/[page]/route.ts
@@ -1,28 +1,29 @@
-import { PROD_URL, SITEMAP_PAGE_SIZE } from "@/lib/constants"
-import { getImaginations } from "@/server/get-Imaginations"
-import { Prisma } from "@prisma/client"
-import { z } from "zod"
+import { Prisma } from "@prisma/client";
+import { PROD_URL, SITEMAP_PAGE_SIZE } from "@/lib/constants";
+import { getImaginations } from "@/server/get-Imaginations";
+import { z } from "zod";
 
 const sitemapContextSchema = z.object({
-  params: z.object({
+ params: z.object({
     page: z.number(),
-  }),
-})
-type SitemapContextProps = z.infer<typeof sitemapContextSchema>
+ }),
+});
 
-export const dynamic = "force-dynamic"
-export const revalidate = 0
+type SitemapContextProps = z.infer<typeof sitemapContextSchema>;
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export async function GET(request: Request, { params }: SitemapContextProps) {
-  const page = params.page
-  const Imaginations = await getImaginations({
+ const page = params.page;
+ const Imaginations = await getImaginations({
     take: SITEMAP_PAGE_SIZE,
     skip: page * SITEMAP_PAGE_SIZE,
-    orderBy: { createdAt: Prisma.SortOrder.asc },
-  })
+    orderBy: { createdAt: 'asc' },
+ });
 
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+ const xml = `<?xml version="1.0" encoding="UTF-8"?>
+ <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${Imaginations
       .map(
         (Imagination: any) => `
@@ -33,14 +34,14 @@ export async function GET(request: Request, { params }: SitemapContextProps) {
     `
       )
       .join("\n")}
-  </urlset>`
+ </urlset>`;
 
-  return new Response(xml, {
+ return new Response(xml, {
     status: 200,
     headers: {
       // Cache for 1 day w/ swr
       "Cache-control": "public, s-maxage=86400, stale-while-revalidate",
       "content-type": "application/xml",
     },
-  })
+ });
 }
