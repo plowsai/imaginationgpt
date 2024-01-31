c4e716ba0acbe2149697e3b4025dda8f0d852233
Jordan Plows <jordan@lightapi.com>
2024-01-30 22:27:42 -0800

    dev: fixing the route under imagination

--- src/app/_components/imagination-grid/index.tsx
+++ src/app/_components/imagination-grid/index.tsx
@@ -29,12 +29,12 @@ export async function ImaginationGrid({ prompt }: ImaginationGridProps) {
     <div className="animate-in fade-in slide-in-from-bottom-4 duration-1200 ease-in-out">
       <h2 className="font-semibold text-md text-left w-full mb-3">{!!prompt ? "Related Imaginations" : "Recent Imaginations"}</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 justify-items-stretch w-full">
-        {/* {Imaginations.map((Imagination) => (
-          <ImaginationCard key={Imagination.id} id={Imagination.id} />
-        ))} */}
-        {Imaginations.map((Imagination: any) => (
+        {Imaginations.map((Imagination) => (
           <ImaginationCard key={Imagination.id} id={Imagination.id} />
         ))}
+        {/* {Imaginations.map((Imagination: ImaginationType) => (
+          <ImaginationCard key={Imagination.id} id={Imagination.id} />
+        ))} */}
       </div>
     </div>
   )
