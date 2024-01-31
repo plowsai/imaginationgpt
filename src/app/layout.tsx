165c90747d54e680c93391382ca3ac14ae6529c6
Jordan Plows <jordan@lightapi.com>
2024-01-30 18:08:19 -0800

    dev: updating the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export function generateMetadata(): Metadata {
     metadataBase: new URL(PROD_URL),
     title,
     description,
-    applicationName: "AI Emojis",
+    applicationName: "Imagination GPT",
     other: {
       "apple-itunes-app": "app-id=6468916301",
     },
@@ -49,7 +49,7 @@ export function generateMetadata(): Metadata {
       title,
       description,
       url: PROD_URL,
-      siteName: "emojis.sh",
+      siteName: "imagination.chat",
       locale: "en_US",
       type: "website",
     },
@@ -57,7 +57,7 @@ export function generateMetadata(): Metadata {
       card: "summary_large_image",
       title,
       description,
-      creator: "@pondorasti",
+      creator: "@plowsai",
     },
   }
 }
@@ -91,11 +91,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
               />
             </svg>
-            <span>emojis</span>
+            <span>Imagination</span>
           </Link>
 
           <div className="flex flex-row flex-nowrap gap-x-1.5 items-center">
-            <Link href="/app?referrer=website" target="_blank" rel="noopener noreferrer">
+            {/* <Link href="/app?referrer=website" target="_blank" rel="noopener noreferrer">
               <span className="sr-only">Download App</span>
               <Image
                 src="/_static/AppStoreBadge.svg"
@@ -105,7 +105,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 priority
                 className="h-8"
               />
-            </Link>
+            </Link> */}
             <Link
               href="https://github.com/pondorasti/emojis"
               target="_blank"
